c9ca284866a887bf6ca551761e02484dc5a07ac6
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-05-16 22:59:52 -0400

    fixed space

--- src/Event.js
+++ src/Event.js
@@ -1,26 +1,26 @@
 import "./Event.css";
 
 function Event({ start_time, end_time, title, index }) {
-    return (
-        <div>
-            {index !== 0 && <hr></hr>}
-            <div style={{ display: "flex", alignItems:"center"}}>
-                <div style={{ width: 180 }}>
-                    <p
-                        style={{
-                            color: "white",
-                            fontWeight: "bold",
-                            fontSize: "20px",
-                            paddingRight: "5px",
-                        }}>
-                        {start_time} {end_time && 
-                        <span> - <br></br>{end_time}</span>}:
-                    </p>
-                </div>
-                <p style={{ color: "white", fontSize: "20px" }}>{title}</p>
-            </div>
+  return (
+    <div>
+      {index !== 0 && <hr></hr>}
+      <div style={{ display: "flex", alignItems:"center"}}>
+        <div style={{ width: 180 }}>
+          <p
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              fontSize: "20px",
+              paddingRight: "5px"
+            }}>
+            {start_time}{end_time && 
+            <span> - <br></br>{end_time}</span>}:
+          </p>
         </div>
-    );
+        <p style={{ color: "white", fontSize: "20px"}}>{title}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Event;
