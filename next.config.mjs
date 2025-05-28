2085fc6cc917ccc3c400c22826929d410c95da83
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-05-28 18:37:24 -0400

    adding workshop signup form

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
         source: "/youtube",
         destination: "https://www.youtube.com/@HacKnight-ly9zq",
         permanent: true,
+      },
+      {
+        source: "/workshop-signup",
+        destination: "https://forms.gle/YPmdMqBVurgyT2Cc6",
+        permanent: false,
       }
     ];
   },
